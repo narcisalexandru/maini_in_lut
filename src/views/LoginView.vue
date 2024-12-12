a6d686380d380c67cff0a9076de9a27a01d23a51
Narcis Cazacu <cazacu.narcis02@gmail.com>
2024-12-12 11:55:36 +0200

    add login UI

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="flex w-full items-center justify-center h-screen">
-    <div class="container w-2/4 py-24 bg-custom flex flex-row rounded-md shadow-xl">
+    <div class="container w-2/4 py-24 bg-custom flex flex-row rounded-none shadow-xl">
       <div class="flex w-1/2 gap-4 justify-center items-center flex-col flex-wrap">
         <InputText name="username" placeholder="Username" type="text" v-model="value"></InputText>
         <Password name="password" placeholder="Password" v-model="valuePass" :feedback="false" />
-        <Button icon="pi pi-user" label="Login"></Button>
+        <Button class="border-none" icon="pi pi-user" label="Login"></Button>
       </div>
       <div>
-        <Divider class="text-red-800" layout="vertical"> OR </Divider>
+        <Divider layout="vertical"> OR </Divider>
       </div>
       <div class="flex w-1/2 justify-center items-center">
-        <Button icon="pi pi-user-plus" label="Sign up"></Button>
+        <Button class="border-none" icon="pi pi-user-plus" label="Sign up"></Button>
       </div>
     </div>
   </div>
@@ -26,11 +26,19 @@ const valuePass = ref(null)
 <style>
 :root {
   --p-divider-content-background: transparent;
+  --p-inputtext-hover-border-color: grey;
+  --p-inputtext-focus-border-color: grey;
+  --p-inputtext-focus-ring-color: #546a50;
+  --p-form-field-focus-ring-shadow: 1px 2px 15px -5px #546a50;
 }
 .p-divider-content {
   z-index: 1;
+  background-color: white;
+  color: #546a50;
+}
+
+.p-button {
   background-color: #546a50;
-  color: #ece4d4;
 }
 
 .p-divider-vertical:before {
@@ -40,10 +48,10 @@ const valuePass = ref(null)
   inset-inline-start: 50%;
   height: 100%;
   content: '';
-  border-inline-start: 1px solid #ece4d4;
+  border-inline-start: 1px solid #546a50;
 }
 
 .bg-custom {
-  background-color: #546a50;
+  background-color: white;
 }
 </style>
